a0122bbf3a191a8c608dc6d8b3f7e0a07c320dfc
keisukesakasai <k6s4i5.3rx@gmail.com>
2023-05-27 17:53:52 +0900

    :poop: Foo

--- main.go
+++ main.go
@@ -25,32 +25,40 @@ func main() {
 	projectID := os.Getenv("PROJECT_ID")
 	fmt.Println("projectID: ", projectID)
 
-	client, err := datastore.NewClient(ctx, projectID)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
+	// clinet 接続から datastore 操作, close までを無限ループで実行
+	for {
+		client, err := datastore.NewClient(ctx, projectID)
+		if err != nil {
+			log.Fatalf("Failed to create client: %v", err)
+		}
 
-	uuid := uuid.New().String()
-	key := datastore.NameKey("entity_k8snovice", uuid, nil)
-	e := new(Entity)
+		uuid := uuid.New().String()
+		key := datastore.NameKey("entity_k8snovice", uuid, nil)
+		e := new(Entity)
+		randomEmoji := getEmoji()
+		e.Value = "Hi! Kubernetes Novice " + randomEmoji
+		loc, _ := time.LoadLocation("Asia/Tokyo")
+		e.CreatedAt = time.Now().In(loc)
+
+		if _, err := client.Put(ctx, key, e); err != nil {
+			log.Fatalf("Failed to save entity: %v", err)
+		}
+
+		e = new(Entity)
+		if err = client.Get(ctx, key, e); err != nil {
+			log.Fatalf("Failed to get entity: %v", err)
+		}
+
+		fmt.Printf("Fetched entity: %v", e)
+
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func getEmoji() string {
 	emojis := []string{"😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇"}
 	rand.Seed(time.Now().UnixNano())
 	randomEmoji := emojis[rand.Intn(len(emojis))]
 
-	e.Value = "Hi! Kubernetes Novice " + randomEmoji
-	loc, _ := time.LoadLocation("Asia/Tokyo")
-	e.CreatedAt = time.Now().In(loc)
-
-	if _, err := client.Put(ctx, key, e); err != nil {
-		log.Fatalf("Failed to save entity: %v", err)
-	}
-
-	e = new(Entity)
-	if err = client.Get(ctx, key, e); err != nil {
-		log.Fatalf("Failed to get entity: %v", err)
-	}
-
-	fmt.Printf("Fetched entity: %v", e)
-
-	time.Sleep(10000000 * time.Second)
+	return randomEmoji
 }
